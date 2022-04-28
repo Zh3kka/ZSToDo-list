85f2bac9c75048a350d7c49d29367c89119b1b64
Zheka <Zh3kaSel@yandex.ru>
2022-04-29 00:15:55 +0300

    update my todo list

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const todoForm = document.querySelector('#todo-form')
-const todoInput = document.querySelector('#todo-input')
-const todoList = document.querySelector('#todo-list')
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
 
 todoForm.addEventListener('submit', formHandler);
 
 function formHandler(e) {
+    // Отменяем обновление формы при клике  
     e.preventDefault()
 
     // Получаем название задачи из инпута
@@ -13,13 +14,25 @@ function formHandler(e) {
     // создаем новый элемент и добавляем задачу из инпута
     const newTask = document.createElement('li');
     newTask.innerHTML = taskText;
-    todoList.append(newTask)
+    todoList.append(newTask);
 
+    // добавление кнопки удалить
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('role', 'img');
+    deleteBtn.innerText = 'Удалить';
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.style['margin-top'] = '15px';
+    newTask.append(deleteBtn);
+
+    // добавляем событие по клику
+    deleteBtn.addEventListener('click', function (e) {  
+        this.closest('li').remove();
+    })
     // очищаем инпут
     todoInput.value = '';
 
-
     // Фокус на инпут
-    todoInput.focus()
-
+    todoInput.focus();
 }
+
+
